dc03711f3e42fece48dcb8880c347f5da1ceebac
norbertattilanagy <norbertattilanagy@gmail.com>
2022-01-06 19:48:26 +0200

    Add project files.

---
+++ Nagy_Norbert_Lab12/Nagy_Norbert_Lab12/Nagy_Norbert_Lab12/ShoppingListDatabase.cs
@@ -0,0 +1,33 @@
+﻿using Nagy_Norbert_Lab12.Data;
+using Nagy_Norbert_Lab12.Models;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Nagy_Norbert_Lab12
+{
+    public class ShoppingListDatabase
+    {
+        private RestService restService;
+
+        public ShoppingListDatabase(RestService restService)
+        {
+            this.restService = restService;
+        }
+
+        internal Task<IEnumerable> GetShopListsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task SaveShopListAsync(ShopList slist)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task DeleteShopListAsync(ShopList slist)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
